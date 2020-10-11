aa2f2c916947047eeefb333842a64917d839e089
Corey Smith <CSSmith1997@gmail.com>
2020-10-10 19:42:24 -0600

    Fixed the JS so that it works (mostly). Removed Bootstrap plug in to do the styling in style.css only. Redid the HTML so that it was cleaner and easier to use in JS.

--- script.js
+++ script.js
@@ -1,102 +1,104 @@
-var question = document.querySelector('#question');
-var choice = document.querSelector(".choice-text");
-var score = 0;
-var scoreText = document.querySelector("#score");
-var currentQuestion = {};
-var questionCounter = 0;
-var availableQuestions = [];
-var startbtn = document.querySelector(".btn1");
+var startBtn = document.getElementById('start-btn');
+var submitBtn = document.getElementById('submit-btn');
+var questionContainer = document.getElementById('question-container');
+var questionEl =document.getElementById('question');
+var answerBtnEl = document.getElementById('answer-buttons');
 var count = 60;
+var score = 0;
+let shuffledQuestions, currentQuestionIndex
+
+startBtn.addEventListener('click', startQuiz)
+submitBtn.addEventListener('click', () =>{
+    currentQuestionIndex++
+    setNextQuestion()
+})
+function startQuiz (){
+console.log('Started')
+startBtn.classList.add('hide')
+shuffledQuestions = questions.sort(() => Math.random() - .5)
+currentQuestionIndex = 0
+questionContainer.classList.remove('hide')
+setNextQuestion()
+
+}
+function setNextQuestion() {
+    resetState()
+showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+function showQuestion(question){
+questionEl.innerText = question.question
+question.answer.forEach(answer => {
+    var button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+        button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerBtnEl.appendChild(button)
+})
+}
+function resetState () {
+    submitBtn.classList.add('Hide')
+    while (answerBtnEl.firstChild) {
+        answerBtnEl.removeChild
+        (answerBtnEl.firstChild)
+    }
+}
+function selectAnswer(e){
+var selectedBtn = e.target
+var correct = selectedBtn.dataset.correct
+if (shuffledQuestions.length > currentQuestionIndex + 1)
+submitBtn.classList.remove('hide')
+}
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+      element.classList.add('correct')
+    } else {
+      element.classList.add('wrong')
+    }
+  }
+  
+  function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+  }
+
+
+
 var questions= 
     [{
         question: "What is the purpose of a for loop?",
-        choices: [
-             "To descibe what something is for",
-             "To create a loop",
-             "To efficiently write a loop that needs to run mulitple times",
-             "To control the flow of JavaScript"],
-            correctChoice: "3"
+        answer: [
+             {text: "To descibe what something is for", correct: false},
+             {text: "To create a loop", correct: false},
+             {text: "To efficiently write a loop that needs to run mulitple times", correct: true},
+             {text: "To control the flow of JavaScript", correct: false}]
     },
     {
         question: "What is the definition of an array?",
-            choices: [
-            "A single variable that is used to store different elements",
-            "A list of objects that you can recall later",
-            "A powerful tool to defeat the enemies of JavaScript",
-            "A collection of elements used in multiple ways throughout your code"],
-            correctChoice: "1"
+            answer: [
+            {text: "A single variable that is used to store different elements", correct: true},
+            {text: "A list of objects that you can recall later", correct: false},
+            {text: "A powerful tool to defeat the enemies of JavaScript", correct: false},
+            {text: "A collection of elements used in multiple ways throughout your code", correct: false}]
     },
 {
     question: "What does HTML stand for?",
-        choices: [
-        "Hyperlink Text Makeup Language",
-        "Hypertext Markup Language",
-        "Hot Tamales Make Lunch",
-        "How To Make Lasers"],
-        correctChoice: "2"
+        answer: [
+            {text: "Hyperlink Text Makeup Language", correct: false},
+            {text: "Hypertext Markup Language", correct: true},
+            {text: "Hot Tamales Make Lunch",correct: false},
+            {text: "How To Make Lasers", correct: false}]
 },
 {
     question: "What is an if/else statement in JavaScript?",
-        choices: [
-        "A statement that checks to see if something is true or false",
-        "A statement that is unsure of what input it needs",
-        "A statement that has no defined properties until given them",
-         "A statement used to perform different actions based on different conditions"],
-        correctChoice: "4"
+        answer: [
+            {text: "A statement that checks to see if something is true or false", correct: false},
+            {text: "A statement that is unsure of what input it needs", correct: false},
+            {text: "A statement that has no defined properties until given them", correct: false},
+            {text: "A statement used to perform different actions based on different conditions", correct: true}
+        ]
         },
 ];
-function displayQuestion (){
-document.getElementById("question").innerHTML = question + 1;
-
-}
-function displayChoices (){
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-function updateTimer (){
-    var currentTime = timeLeft - timeElapsed
-    timeElapsed.textContent = currentTime;
-}
-function startCount(){
-    timer = setInterval(function (){
-        timeGauge.innterHTML = "Time: " + count--;
-        if (count === -1) clearInterval(timer);
-    }, 1000);
-}
-function setChoices (questionNum){
-    questions[questionNum].choices.forEach(function (choice, i){
-        var selectorNum = i + 1;
-        var selection = document.querySelector('[data-number=' + '"' + 
-        selectorNum + '"]').innterHTML = choice
-        console.log(choice, selectorNum, selection)});
-
-}
-
-function saveScores() {
-    var initialsEl = document.getElementById("initials");
-    var newScore = {
-        initials:initialsEl.value,
-        highScore: score
-    };
-    console.log(newScore);
-    highScore.push(newScore);
-    console.log(highScore);
-    localStorage.setItem("scores", JSON.stringify(highScore));
-}
-submitButton.addEventListener("click", saveScores);
-
-document.addEventListener("DOMContentLoaded", function (event){
-    startCount(60);
-    setChoices(questionCounter);
-})
